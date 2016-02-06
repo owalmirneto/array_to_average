a3c3129d41f59059bec21a5a38ded1f8610d0542
Walmir Neto <wfsneto@gmail.com>
2016-02-06 16:29:31 -0300

    rename methods

--- lib/array_to_average/calculate.rb
+++ lib/array_to_average/calculate.rb
@@ -1,12 +1,14 @@
 module ArrayToAverage
   class Calculate
+    attr :numbers
+
     def initialize numbers
       @numbers = numbers.kind_of?(Array) ? numbers : []
       @sum = @numbers.inject { |sum,number| sum ? sum + number.to_f : number.to_f } || 0
       @size = @numbers.empty? ? 1 : @numbers.size
     end
 
-    def calculate
+    def average
       @sum / @size
     end
   end
--- spec/array_to_average/calculate_spec.rb
+++ spec/array_to_average/calculate_spec.rb
@@ -5,19 +5,19 @@ module ArrayToAverage
     let(:numbers) { [7,8,5] }
     let(:average) { 20.0 / numbers.size }
 
-    describe '.calculate' do
+    describe '.average' do
       it 'when numbers is not empty' do
-        calculated = ArrayToAverage::Calculate.new(numbers).calculate
+        calculated = ArrayToAverage::Calculate.new(numbers).average
         expect(calculated).to eq average
       end
 
       it 'when numbers is empty' do
-        calculated = ArrayToAverage::Calculate.new([]).calculate
+        calculated = ArrayToAverage::Calculate.new([]).average
         expect(calculated).to eq 0.0
       end
 
       it 'when numbers is array' do
-        calculated = ArrayToAverage::Calculate.new(100).calculate
+        calculated = ArrayToAverage::Calculate.new(100).average
         expect(calculated).to eq 0.0
       end
     end
